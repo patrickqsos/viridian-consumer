5486431f9ac87ef7c793dc1dd4c1831fac84ab5d
Patricio Quispe Sosa <Patrick@MacBook-Pro-de-Patricio.local>
2018-09-05 23:10:04 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace viridian_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
