c83fa8a3a58fd8aa5ed9fdcf21ffaf157e3e50bf
Patricio Quispe Sosa <Patrick@MacBook-Pro-de-Patricio.local>
2018-09-06 20:51:55 -0400

    Se agregó logica para subscribirse a los mensajes de un topic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
 
 namespace viridian_consumer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "sample-consumer" },
+                { "bootstrap.servers", "localhost:9092" },
+                { "enable.auto.commit", "false"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {                
+                consumer.Subscribe(new string[]{"viridian-test"});
+
+                consumer.OnMessage += (_, msg) => 
+                {
+                    Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    consumer.CommitAsync(msg);
+                };
+
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+            }
         }
     }
 }
